8d7d520c1d2c60eaf1076c5f9303dc24126f1dc5
Benjamin Larsen <benlarsendk@gmail.com>
2020-02-04 13:55:54 +0100

    create file

---
+++ setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+
+# Install requirements
+apt install libssl-dev build-essential autoconf libtool pkg-config -y
+
+
+
+
+# Download IBM Software TPM and TSS
+cd /tmp/
+wget -O ibmtpm.tar.gz https://sourceforge.net/projects/ibmswtpm2/files/latest/download
+wget -O ibmtss.tar.gz https://sourceforge.net/projects/ibmtpm20tss/files/latest/download
+
+
+mkdir /opt/ibmtpm && tar -xzf ibmtpm.tar.gz -C /opt/ibmtpm
+mkdir /opt/ibmtss && tar -xzf ibmtss.tar.gz -C /opt/ibmtss
+
+
+# Build IBM TPM
+cd /opt/ibmtpm/src
+make
+
+cd /opt/ibmtss
+autoreconf -i
+./configure --prefix=${HOME}/local --disable-hwtpm
+make clean
+cd utils
+make -f makefiletpmc
